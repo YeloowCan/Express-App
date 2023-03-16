0f21abf423a54c5a19f169da5354eab7138f9a11
YeloowCan <942789838@qq.com>
2023-03-16 23:04:22 +0800

    post

--- index.js
+++ index.js
@@ -1,9 +1,30 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.all('*', (req, res, next) => {
+  // 设置允许跨域的域名,*代表允许任意域名跨域
+  res.header('Access-Control-Allow-Origin', '*');
+  // 允许的header类型
+  res.header('Access-Control-Allow-Headers', 'content-type');
+  // 跨域允许的请求方式
+  res.header('Access-Control-Allow-Methods', 'DELETE,PUT,POST,GET,OPTIONS');
+  next();
+});
+
 app.get('/', (req, res) => {
   res.send('Hello World');
+});
+
+app.post('/user', (req, res) => {
+  res.send({
+    code: 200,
+    msg: req.body,
+  });
 });
 
 app.listen(port, () => {
